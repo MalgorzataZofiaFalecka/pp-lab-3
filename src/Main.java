bc2a62e5fa63f8c68fc91bd9f166e31cdc337448
Małgorzata Falecka <malgorzata.falecka.mail@gmail.com>
2024-04-07 15:33:42 +0200

    Zad2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        // Tworzenie obiektu klasy Point i inicjowanie go wartościami
-        Point point = new Point(3.5, 2.0);
+import geometry.*;
+    public class Main {
+        public static void main(String[] args) {
 
-        // Wyświetlanie współrzędnych punktu
-        System.out.println("Współrzędne punktu:");
-        System.out.println("x = " + point.getX());
-        System.out.println("y = " + point.getY());
+            Point center = new Point(3.0, 4.0);
+   
+           
+            Circle circle = new Circle(center, 5.0);
+            
+            System.out.println("Właściwości koła:");
+            System.out.println("Centrum koła: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+            System.out.println("Promień koła: " + circle.getRadius());
+            System.out.println("Obwód koła: " + circle.calculatePerimeter());
+            System.out.println("Pole powierzchni koła: " + circle.getArea());
+        }
     }
-}
